a7ced84501d35b3c6b1e985e6b37aa754f49c586
Kyle Schneider <kyle.is.gfy@gmail.com>
2020-08-08 07:57:34 -0700

    Initial Commit

---
+++ Theia/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Theia
+//
+//  Created by Kyle Schneider on 8/8/20.
+//  Copyright © 2020 Kyle Schneider. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Theia/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Theia
+//
+//  Created by Kyle Schneider on 8/8/20.
+//  Copyright © 2020 Kyle Schneider. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
